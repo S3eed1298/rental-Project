864dfff9c55d0d3590fc9dc4b4ee03c695dc7b3b
U190709713 <s3eed1298@gmail.com>
2022-02-03 14:29:41 +0300

    Add project files.

---
+++ rental Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rental_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
