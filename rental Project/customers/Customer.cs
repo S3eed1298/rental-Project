cd2984ea9b20ec90e990775d82e1081caf5bd62a
Arda Can <ardacan271@gmail.com>
2022-02-03 15:36:25 +0300

    test2

--- rental Project/customers/Customer.cs
+++ rental Project/customers/Customer.cs
@@ -8,5 +8,8 @@ namespace rental_Project.customers
 {
     class Customer
     {
+        private string car_model;
+        private int car_model_year;
+        private int car_base_price;
     }
 }
